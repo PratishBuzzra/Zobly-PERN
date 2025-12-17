53b3fbec222a3bb4a69ac37a69ee71163d19b96c
PratishBuzzra <bajracharyapratish148@gmail.com>
2025-12-17 20:47:57 +0545

    handle login and logout

--- Frontend/src/components/Layout/Navbar.jsx
+++ Frontend/src/components/Layout/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Link, NavLink} from "react-router-dom"
-
+import { AuthContext } from '../../Context/authContext'
+AuthContext
 const Navbar = () => {
+  const {isLoggedIn, username, logout} = useContext(AuthContext)
   return (
     <div className='fixed top-0 left-0 w-full bg-white shadow-lg px-4 py-4 z-50'>
         <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -26,9 +28,17 @@ const Navbar = () => {
                   <li>All Jobs</li>
                 </NavLink>
               </ul>
-               <Link to={"/register"}>
+              {isLoggedIn ? (
+                <div className='flex items-center gap-4'>
+                  <span>Loggin as <span className='font-semibold'>{username}</span></span>
+                  <button onClick={logout} className='bg-blue-600 px-3 py-1 text-white rounded cursor-pointer'>Logout</button>
+                </div>
+              ):(
+              <Link to={"/register"}>
                 <button className='bg-blue-600 px-3 py-1 text-white rounded cursor-pointer'>Register</button>
                 </Link>
+              )}
+               
 
             </nav>
         </div>
